ff6a2b6682ccb94522a51c13239417b877818754
Tati Marksman <sdushor95@mail.ru>
2024-11-23 15:07:08 +0300

    Reveal complited

---
+++ dom/reveal/task.js
@@ -0,0 +1,13 @@
+function handleScroll() {
+    const reveals = document.querySelectorAll('.reveal');
+    reveals.forEach(reveal => {
+        const rect = reveal.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            reveal.classList.add('reveal_active');
+        } else {
+            reveal.classList.remove('reveal_active');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
